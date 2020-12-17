df8461eb49c37fef47e47dc51d1ac23b4b8b1798
NullDefault <davisha999@gmail.com>
2020-12-17 16:35:46 -0500

    pdf files working from public

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import {ThemeProvider, CSSReset, ColorModeProvider, theme} from '@chakra-ui/react'
 import {ProvideAuth} from "../lib/auth";
+import {pdfjs} from "react-pdf";
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 function MyApp({Component, pageProps}) {
     return (
--- src/pages/issues/[year].js
+++ src/pages/issues/[year].js
@@ -1,9 +1,23 @@
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router';
+import {Document, Page} from 'react-pdf';
+import {Container} from "../../components/Container";
+import {useState} from "react";
 
 const Issue = () => {
     const router = useRouter()
-    const { year } = router.query
-    return <p>Drew Review { year }</p>
+    const {year} = router.query
+    const filename = `/${year}.pdf`
+
+    const [pageNum, setPage] = useState(3)
+    console.log(pageNum)
+    return (
+        <Container>
+            <Document file={filename} renderMode={"canvas"}>
+                <Page pageNumber={pageNum}>
+                </Page>
+            </Document>
+        </Container>
+    )
 }
 
 export default Issue
