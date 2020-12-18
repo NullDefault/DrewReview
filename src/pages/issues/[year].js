e3bca0718616db0dfbe881666b2111878bab7f84
NullDefault <davisha999@gmail.com>
2020-12-18 11:12:39 -0500

    added some styling

--- src/pages/issues/[year].js
+++ src/pages/issues/[year].js
@@ -3,7 +3,7 @@ import {Document, Page} from 'react-pdf';
 import {Container} from "../../components/Container";
 import {useState} from "react";
 import {Button, Center} from "@chakra-ui/react"
-import {Box} from "@chakra-ui/layout";
+import {Box, HStack} from "@chakra-ui/layout";
 import {ChevronLeftIcon, ChevronRightIcon} from "@chakra-ui/icons";
 
 const Issue = () => {
@@ -14,28 +14,29 @@ const Issue = () => {
     const [pageNum, setPage] = useState(3)
 
     const onRightClick = () => {
-        setPage(pageNum+1);
+        setPage(pageNum + 1);
     }
     const onLeftClick = () => {
-        setPage(pageNum-1);
+        setPage(pageNum - 1);
     }
 
     return (
         <Center>
-            <Button colorScheme="teal" onClick={onLeftClick}>
-                <ChevronLeftIcon/>
-            </Button>
-            <Box>
-                <Document file={filename}>
-                    <Page pageNumber={pageNum}>
-                    </Page>
-                </Document>
-            </Box>
-            <Button colorScheme="teal" onClick={onRightClick}>
-                <ChevronRightIcon/>
-            </Button>
+            <HStack spacing="24px" bg="blue.800">
+                <Button colorScheme="green" onClick={onLeftClick} ml="24px">
+                    <ChevronLeftIcon/>
+                </Button>
+                <Box py="36px">
+                    <Document file={filename}>
+                        <Page pageNumber={pageNum} size="A4">
+                        </Page>
+                    </Document>
+                </Box>
+                <Button colorScheme="green" onClick={onRightClick} mr="24px">
+                    <ChevronRightIcon/>
+                </Button>
+            </HStack>
         </Center>
-
     )
 }
 
