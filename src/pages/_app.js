5ca26ef35ef7e1c16345f40f9884e90e1e14c9f1
NullDefault <davisha999@gmail.com>
2020-12-20 17:51:21 -0500

    forgot to add files for last commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,5 @@
 import {ThemeProvider, CSSReset, ColorModeProvider, theme} from '@chakra-ui/react'
 import {ProvideAuth} from "../lib/auth";
-import {pdfjs} from "react-pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 function MyApp({Component, pageProps}) {
     return (
--- src/pages/issues/[year].js
+++ src/pages/issues/[year].js
@@ -1,9 +1,11 @@
 import {useRouter} from 'next/router';
-import {Document, Page} from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
 import {useState} from "react";
 import {Button, Center} from "@chakra-ui/react"
 import {Box, HStack} from "@chakra-ui/layout";
 import {ChevronLeftIcon, ChevronRightIcon} from "@chakra-ui/icons";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.min.js`
 
 const Issue = () => {
     const router = useRouter()
@@ -12,7 +14,7 @@ const Issue = () => {
     // The naming pattern for the pdf files is DrewReview_V{n}.pdf
     // There is no issue 0, as the first issue is for the year 2008 and is titled DrewReview_V1.pdf
     // Consequently, to get the right version number, you need to take the query year and subtract 2007 from it
-    const filename = `/DrewReview_V${year-2007}.pdf`
+    const filename = `/DrewReview_V${year - 2007}.pdf`
 
     const [pageNum, setPage] = useState(3)
 
