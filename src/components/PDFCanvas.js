05186873bd6c3170aedabb6c7fa1a21c23ed0723
NullDefault <davisha999@gmail.com>
2020-12-30 15:00:21 -0500

    Pdff stoof

--- src/components/PDFCanvas.js
+++ src/components/PDFCanvas.js
@@ -17,22 +17,49 @@ import {BackgroundContainer} from "./BackgroundContainer";
 
 export const PDFCanvas = ({filename}) => {
     const [width, height] = useWindowSize();
-    const [pageNum, setPage] = useState(2);
+    const [pageDimensions, updatePageDimensions] = useState({width: 0, height: 0})
+    const [pageNum, setPage] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
     const progress = pageNum / (totalPages / 100);
 
     const onDocumentLoadSuccess = ({numPages}) => {
         setTotalPages(numPages);
+        resizeCanvas();
     };
 
     const pageDecrement = (pageNum <= 0) ? <div/> : <NumberDecrementStepper/>
     const pageIncrement = (pageNum >= totalPages) ? <div/> : <NumberIncrementStepper/>
 
+    const resizeCanvas = () => {
+        updatePageDimensions({width: width * .75, height: height * .75});
+    }
+
     const setNewPage = (value) => {
         value = parseInt(value);
-        if (value !== 0 && value <= totalPages) {
+        if (Math.abs(value - pageNum) === 1) {
+            if (value < pageNum) {
+                turnLeftPage();
+            } else {
+                turnRightPage();
+            }
+        } else {
             setPage(value);
+            resizeCanvas();
         }
+    }
+
+    const turnLeftPage = () => {
+        if (pageNum - 1 !== 0) {
+            setPage(pageNum - 1);
+        }
+        resizeCanvas();
+    }
+
+    const turnRightPage = () => {
+        if (pageNum + 1 <= totalPages) {
+            setPage(pageNum + 1);
+        }
+        resizeCanvas();
     }
 
     const pageNumberInput = <NumberInput value={pageNum} allowMouseWheel={true} onChange={setNewPage}>
@@ -42,22 +69,28 @@ export const PDFCanvas = ({filename}) => {
         </NumberInputStepper>
     </NumberInput>
 
-    const loadingBg = <BackgroundContainer width={width / 2} height={height / 2} wrap={true}/>
+    let loadingBg = <BackgroundContainer width={pageDimensions.width} height={pageDimensions.height} wrap={true}/>
+
+    const pageRender = <Page
+        pageNumber={pageNum}
+        height={pageDimensions.height}
+        loading={loadingBg}
+        wrap={true}/>
+
 
     return (
         <VStack>
             <HStack spacing="24px" justifyContent={'center'}>
-                <PageButton isLeft={true} setPage={setPage} pageNum={pageNum} totalPages={totalPages}/>
+                <PageButton isLeft={true} setPage={turnLeftPage}/>
                 <Box pt="36px">
                     <Document file={filename} onLoadSuccess={onDocumentLoadSuccess} loading={loadingBg}
                               onSourceSuccess={() => {
                                   setPage(2)
                               }}>
-                        <Page pageNumber={pageNum} width={width / 2} height={height / 2} loading={loadingBg}
-                              wrap={true}/>
+                        {pageRender}
                     </Document>
                 </Box>
-                <PageButton isLeft={false} setPage={setPage} pageNum={pageNum} totalPages={totalPages}/>
+                <PageButton isLeft={false} setPage={turnRightPage}/>
             </HStack>
             <Box w={width / 2} h="100%" pb="36px">
                 <Progress value={progress}/>
--- src/components/PageButton.js
+++ src/components/PageButton.js
@@ -1,25 +1,14 @@
 import {Button} from "@chakra-ui/react";
 import {ChevronLeftIcon, ChevronRightIcon} from "@chakra-ui/icons";
 
-export const PageButton = ({isLeft, setPage, pageNum, totalPages}) => {
-    const onRightClick = () => {
-        if(pageNum !== totalPages){
-            setPage(pageNum + 1);
-        }
-
-    }
-    const onLeftClick = () => {
-        if(pageNum !== 1){
-            setPage(pageNum - 1);
-        }
-    }
+export const PageButton = ({isLeft, setPage}) => {
 
     const icon = isLeft ? <ChevronLeftIcon/> : <ChevronRightIcon/>;
 
     return (
         <Button style={{borderRadius: '24px'}} colorScheme="blue"
                 onClick={
-                    isLeft ? onLeftClick : onRightClick
+                    setPage
                 }
                 ml={
                     isLeft ? '24px' : '0px'
