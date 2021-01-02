829d528fa009784fddc3ab2a7a46167383f0fbb8
NullDefault <davisha999@gmail.com>
2021-01-01 19:02:44 -0500

    removed unnecessary icon files

--- src/components/TopNavBar/MenuToggle.js
+++ src/components/TopNavBar/MenuToggle.js
@@ -1,11 +1,11 @@
 import {Box} from "@chakra-ui/react";
-import {MenuIcon} from "../icons/MenuIcon";
-import {CloseIcon} from "../icons/CloseIcon";
+import {CloseIcon, HamburgerIcon} from "@chakra-ui/icons";
 
 export const MenuToggle = ({toggle, isOpen}) => {
+    const icon = isOpen ? <CloseIcon color="white" size="lg"/> : <HamburgerIcon color="white" size="lg"/>
     return (
         <Box display={{base: "block", md: "none"}} onClick={toggle}>
-            {isOpen ? <CloseIcon/> : <MenuIcon/>}
+            {icon}
         </Box>
     );
 };
