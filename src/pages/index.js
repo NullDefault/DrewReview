a24d48aacd54344d821b09c5d3b4dd8499a338ff
NullDefault <davisha999@gmail.com>
2021-07-20 14:41:13 -0400

    fixing weird mobile bugs. Apple fix ur shit ffs

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,34 +15,41 @@ const Index = () => {
       width={["90vw", "80vw", "65vw", "70vw", "75vw", "80vw"]}
       height="100vh"
       my="1rem"
+      mx="auto"
     >
       <object
         data={filename}
         type="application/pdf"
         style={{
-          display: "block",
           width: "100%",
-          height: "95%"
+          height: "95%",
         }}
-      ></object>
+      />
     </Box>
   );
 
+  const mobileLink = isMobile ? (
+    <Box>
+      <Text p="12px" color="white">
+        If your web browser doesn't have a PDF plugin you can instead{" "}
+        <Link color="blue.300" href={filename}>
+          click here to view the PDF file.
+        </Link>
+      </Text>
+    </Box>
+  ) : (
+    <></>
+  );
+
   return (
-    <>
-      {isMobile ? (
-        <Box>
-          <Text p="12px" color="white">
-            If your web browser doesn't have a PDF plugin you can instead{" "}
-            <Link color="blue.300" href={filename}>
-              click here to view the PDF file.
-            </Link>
-          </Text>
+    <Box>
+      <Box h="100%">
+        <Box h="100%" textAlign="center">
+          {mobileLink}
         </Box>
-      ) : (
-        <></>
-      )}
-      <VStack>{pdfView}</VStack>
+        <Box>{pdfView}</Box>
+      </Box>
+
       <Box bg="white" border="5px solid" borderColor="black" mx="10vw">
         <Text
           fontSize={{ base: "xl", md: "2xl" }}
@@ -67,8 +74,8 @@ const Index = () => {
           intellectual vibrancy of the students of the College of Liberal Arts.
         </Text>
       </Box>
-      <Box h={8} />
-    </>
+      <Box h="2rem"/>
+    </Box>
   );
 };
 
